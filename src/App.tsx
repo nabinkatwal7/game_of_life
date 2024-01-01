f8080bb8de4c0d69b02f2b1124ea76bfe2af1862
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-01 12:25:30 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {FC, useState} from "react";
+import './App.css';
+
+const numRows = 25;
+const numCols = 35;
+const App: FC = () => {
+    const [grid, setGrid] = useState()
+}
+
+export default App;
